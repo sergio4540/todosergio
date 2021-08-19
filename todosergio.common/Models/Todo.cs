abc4037f02a327cc8cdf1a969bc7319d71465f07
sergio4540 <sergio4540@hotmail.com>
2021-08-19 15:28:52 -0500

    Add entity

---
+++ todosergio.common/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace todosergio.common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+
+    }
+}
