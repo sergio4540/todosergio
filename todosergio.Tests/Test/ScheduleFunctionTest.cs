a7961d6404cc1e3b0326af49bbd137eb4114dbd9
sergio4540 <sergio4540@hotmail.com>
2021-08-24 09:59:21 -0500

    unit test

---
+++ todosergio.Tests/Test/ScheduleFunctionTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using todosergio.Functions.Functions;
+using todosergio.Tests.Helpers;
+using Xunit;
+
+namespace todosergio.Tests.Test
+{
+    public class ScheduleFunctionTest
+    {
+        [Fact]
+        public void ScheduleFunctionTest_Should_Log_Message()
+        {
+
+            //Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+           ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+            //Act
+            ScheduleFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            //Asert
+            Assert.Contains("Deleting completed", message);
+
+        }
+    }
+}
