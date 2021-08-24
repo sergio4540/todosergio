47e4216e86490fa43d8745b6b86fa5be003aa0e8
sergio4540 <sergio4540@hotmail.com>
2021-08-24 08:56:05 -0500

    Unit test preparation

---
+++ todosergio.Tests/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todosergio.Tests.Helpers
+{
+   public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
---
+++ todosergio.Tests/Helpers/NullScope.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace todosergio.Tests.Helpers
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+        public void Dispose() { }
+        private NullScope() { }
+    }
+}
